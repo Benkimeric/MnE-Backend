7b93e83d7ab3fb31a29c29e03cea6ab44043e06b
Bolton <bolton.otieno.com>
2022-07-10 14:12:30 +0300

    fix error message

--- src/modules/households/householdsController.js
+++ src/modules/households/householdsController.js
@@ -1,5 +1,5 @@
-import ResponseHandler from '../../helpers/responseHandler';
-import HouseholdHelper from '../../helpers/households';
+import ResponseHandler from "../../helpers/responseHandler";
+import HouseholdHelper from "../../helpers/households";
 
 class HouseholdsController {
   static async getHouseholds(req, res) {
@@ -8,15 +8,15 @@ class HouseholdsController {
       const whereClause = {
         where: {
           id: householdId,
-        }
+        },
       };
       const query = householdId ? whereClause : null;
       const households = await HouseholdHelper.findHouseholds(query);
       const data = householdId ? { household: households[0] } : { households };
-      const message = `Household${householdId ? '' : 's'} fetched successfully`;
+      const message = `Household${householdId ? "" : "s"} fetched successfully`;
       return ResponseHandler.handleSuccess(message, data, 200, res);
     } catch (error) {
-      return ResponseHandler.handleError('Server Error', 500, res);
+      return ResponseHandler.handleError("Server Error", 500, res);
     }
   }
 
@@ -34,14 +34,19 @@ class HouseholdsController {
       const householdIds = households.map((household) => household.uniqueId);
       const sortedIds = householdIds.sort((a, b) => b.localeCompare(a));
       const newMiddleId = HouseholdHelper.incrementMiddleId(sortedIds[0]);
-      const uniqueId = [code, newMiddleId, year].join('/');
-      const household = await HouseholdHelper.createHousehold({ ...body, uniqueId });
+      const uniqueId = [code, newMiddleId, year].join("/");
+      const household = await HouseholdHelper.createHousehold({
+        ...body,
+        uniqueId,
+      });
       return ResponseHandler.handleSuccess(
-        'Household created successfully',
-        { household }, 201, res
+        "Household created successfully",
+        { household },
+        201,
+        res
       );
     } catch (error) {
-      return ResponseHandler.handleError('Server Error', 500, res);
+      return ResponseHandler.handleError("Server Error", 500, res);
     }
   }
 
@@ -49,15 +54,18 @@ class HouseholdsController {
     try {
       const { body } = req;
       const { householdId } = req.params;
-      const household = await HouseholdHelper.updateHousehold(body, householdId);
+      const household = await HouseholdHelper.updateHousehold(
+        body,
+        householdId
+      );
       return ResponseHandler.handleSuccess(
-        'Household updated successfully',
+        "Household updated successfully",
         { household },
         200,
         res
       );
     } catch (error) {
-      return ResponseHandler.handleError('Server Error', 500, res);
+      return ResponseHandler.handleError("Server Error", 500, res);
     }
   }
 
@@ -67,13 +75,13 @@ class HouseholdsController {
       const household = await HouseholdHelper.destroyHousehold(householdId);
 
       return ResponseHandler.handleSuccess(
-        'Household deleted successfully',
+        "Household deleted successfully",
         { household },
         200,
         res
       );
     } catch (error) {
-      return ResponseHandler.handleError('Server Error', 500, res);
+      return ResponseHandler.handleError("Server Error", 500, res);
     }
   }
 
@@ -82,17 +90,17 @@ class HouseholdsController {
       const { householdId } = req.params;
       const household = await HouseholdHelper.restoreHousehold(householdId);
       const message = household
-        ? 'Household restored successfully' : 'Household with the given Id does not exist';
+        ? "Household restored successfully"
+        : "Household with the given Id does not exist";
       const status = household ? 200 : 404;
 
-      return ResponseHandler.handleSuccess(
-        message,
-        { household },
-        status,
-        res
-      );
+      if (!household) {
+        return ResponseHandler.handleError(message, status, res);
+      }
+
+      return ResponseHandler.handleSuccess(message, { household }, status, res);
     } catch (error) {
-      return ResponseHandler.handleError('Server Error', 500, res);
+      return ResponseHandler.handleError("Server Error", 500, res);
     }
   }
 }
